84f1958d4b8e098884e35f9431ede79221d5320e
Potterf Mária <potterf@fld.czu.cz>
2025-03-11 14:20:45 +0100

    read the aliases from teh files name

--- teast_read_full_protocol.R
+++ teast_read_full_protocol.R
@@ -4,45 +4,30 @@
 
 library(sf)        # For spatial data handling
 library(terra)     # For `vect()` method
-library(RSQLite)   # For accessing GPKG metadata
-
-# read metadata:
-
-# Connect to the GPKG file as a SQLite database
-con <- dbConnect(RSQLite::SQLite(), "raw/data_SVK_protocol.gpkg")
-
-# List all tables in the GPKG (find the correct metadata table)
-dbListTables(con)
-
-dbGetQuery(con, "PRAGMA table_info('rap_ass_v2_a021dd1f_4cf0_4c71_ae0a_6cf6431fda28')")
-
-# Check for alias data
-aliases_data <- dbGetQuery(con, "SELECT * FROM log_added_attrs")
-print(aliases_data)
-
-search_aliases <- function(con) {
-  tables <- dbListTables(con)
-  results <- list()
-  
-  for (table in tables) {
-    query <- paste("SELECT * FROM", table)
-    try({
-      data <- dbGetQuery(con, query)
-      if (any(grepl("alias|description|comment", names(data), ignore.case = TRUE))) {
-        results[[table]] <- data
-      }
-    }, silent = TRUE)
-  }
-  
-  results
-}
-
-alias_data <- search_aliases(con)
-print(alias_data)
-
+library(dplyr)
+library(tidyr)
+library(data.table)
 
 # read data
 #path = "C:/Users/potterf/OneDrive - CZU v Praze\Dokumenty\2025_CZU_postbrouk\example_from_Christian\full_data_for_tablet\slk""
 protocol <- vect("raw/data_SVK_protocol.gpkg")
+aliases <- vect("raw/data_SVK_protocol_aliases.gpkg")
+#aliases <- fread("raw/test_CSV.csv")
 
 head(protocol)
+head(aliases)
+
+ncol(protocol)  # 326
+ncol(aliases)   # 175
+
+# tehy do not fit: but the questions are enought to export
+aliases_df <- as.data.frame(aliases)
+class(t(aliases_df))
+
+# Convert vector to data frame
+aliases_clean <- data.frame(Alias = colnames(aliases_df))
+
+# export
+fwrite(aliases_clean, 'outTable/aliases_names.csv')
+
+
