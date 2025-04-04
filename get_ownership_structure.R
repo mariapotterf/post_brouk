3b49a5827990099befdcc30cb57b6225b70a1b0a
Potterf Mária <potterf@fld.czu.cz>
2025-04-04 16:03:22 +0200

    clean up data: limit to C4

--- get_ownership_structure.R
+++ get_ownership_structure.R
@@ -60,7 +60,7 @@ company_clean$owner <- 'company'
 state_clean$owner   <- 'state'
 
 # simplify
-my_tolerance = 20
+my_tolerance =5
 private_simplified  <- simplifyGeom(private_clean,  tolerance = my_tolerance)
 unknown_simplified  <- simplifyGeom(unknown_clean,  tolerance = my_tolerance)
 village_simplified  <- simplifyGeom(village_clean,  tolerance = my_tolerance)
@@ -80,19 +80,46 @@ aoi_3035        <- project(aoi, 'EPSG:3035')  # Transforms coordinates
 subplots_3035   <- project(subplots,  'EPSG:3035')
 crs(aoi_3035)
 
+## PLOT data on map ---------------------------------
+
+# Clean up field data: keep only points within AOI -----------------
+
+
+subplots_in_aoi     <- crop(subplots_3035, aoi_3035)
+subplots_with_owner <- terra::extract(all_owners_3035, subplots_in_aoi)
+# intersect subplots with ownership structure
+
+# 2. Add 'ID' column to subplots (needed for join)
+subplots_in_aoi$row <- 1:nrow(subplots_in_aoi)
+
+# 3. Join extracted attributes back to the point layer using ID
+subplots_with_owner <- merge(subplots_in_aoi, ownership_info, by.x = "ID", by.y = "ID")
+
+round(table(subplots_with_owner$owner)/5,0)
 
 
 # convert to sf for ggplot
 owners_sf <- st_as_sf(all_owners_3035)
 aoi_sf    <- st_as_sf(aoi_3035)
-subplots_sf    <- st_as_sf(subplots_3035)
+subplots_sf    <- st_as_sf(subplots_in_aoi)
+
+# add ownership data:
+subplots_sf <- subplots_sf %>% 
+  full_join(subplots_with_owner)
 
 crs(owners_sf) == crs(subplots_sf)
 
 ggplot() +
-  geom_sf(data = aoi_sf, fill ="white", color = "black") +
-  geom_sf(data = owners_sf, aes(fill = owner), color = NA) +
-  geom_sf(data = subplots_sf, color = "red") +
-  scale_fill_brewer(palette = "Set3") +
+  geom_sf(data = owners_sf, aes(fill = owner), color = NA, lwd = 1.2) +
+  geom_sf(data = subplots_sf, color = "white", size = 3) +
+   geom_sf(data = subplots_sf, aes(fill = owner_type), color = 'black', shape = 21,  size = 2, stroke = 0.8) +
+  geom_sf(data = aoi_sf, fill =NA, color = "black") +
+ # scale_fill_brewer(palette = "Set3") +
   coord_sf(crs = st_crs(3035)) +  # ← forces plot to stay in EPSG:3035
   theme_void()
+
+
+# export shp with ownership types: 
+
+
+writeVector(subplots_in_aoi, "outData/subplots_by_owner.gpkg", filetype = "GPKG", overwrite = TRUE)
