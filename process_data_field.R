bfddabde9263a3e9a19435a47a29e41171d8e9bb
Potterf Mária <potterf@fld.czu.cz>
2025-06-11 16:00:48 +0200

    set up data processing

---
+++ process_data_field.R
@@ -0,0 +1,12 @@
+
+
+# decode field data
+# what species?
+# what stem density?
+# heights?
+# damages?
+# make a cluster based on 
+
+# read gpkg - has location as point, and additional 
+
+library(terra)
