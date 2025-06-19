f9a1c535cecb5808d3972a8b16bc749617ff1b0a
Potterf Mária <potterf@fld.czu.cz>
2025-06-19 10:44:02 +0200

    get iland species color code

---
+++ help/iLand_species_colors.R
@@ -0,0 +1,39 @@
+
+# iLand species colors from Marco
+
+species.we.have<-unique(lnd$species)
+
+cols.all=c( "rops"="#e0e0e0", "acpl"="#A9A9A9",   "alin"="#696969", "alvi"="#2e2e2e",
+            "bepe"="#fadfad", 
+            "casa"="#7eeadf", "coav"="#20c6b6",  
+            "tipl"="#645394", "ulgl"="#311432" ,
+            "saca"="#D8BFD8",  "soar"="#DDA0DD", "soau"="#BA55D3",
+            "pice"="#D27D2D", "pini"="#a81c07",
+            "algl"="#2ECBE9","tico"="#128FC8",  "potr"="#00468B","poni"="#5BAEB7",
+            "frex"="#fe9cb5","cabe"="#fe6181","acps"="#fe223e",
+            "lade"="#FFFE71","abal"="#FFD800", "pisy"="#A4DE02",
+            "fasy"="#76BA1B", "piab"="#006600",
+            "quro"="#FF7F00", "qupe"="#FF9900", "qupu"="#CC9900" 
+)
+
+new_order_gg.all=c("alvi","alin", "acpl", "rops","bepe" ,"coav", "casa", "ulgl", "tipl",  "soau", "soar", "saca",  "pini", "pice",
+                   "poni", "algl", "tico", "potr",  "frex","cabe", "acps",  "lade", "abal",  "qupu", "qupe","quro","pisy", "fasy", "piab")
+
+
+# This will show at the end only the species we really have on the landscape. 
+
+cols<-cols.all[names(cols.all) %in% species.we.have]
+new_order_gg<- new_order_gg.all[new_order_gg.all %in% species.we.have]
+
+
+#-----------------------------------------
+g1 <- ggplot(landscape_cc, aes(year,volume_m3, fill=factor(species, levels=new_order_gg)))+
+  geom_area() +
+  scale_fill_manual(values=cols[new_order_gg], guide=guide_legend(reverse=TRUE))+
+  ggtitle("Clear Cut")+
+  labs(x = "Year",y="Volume m3/ha",fill = "Species")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  ylim(0,350)+
+  theme_bw()
+
+g1
