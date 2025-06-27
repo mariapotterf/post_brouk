690a70bd0cccf9fcc720c7e2d91f289719859ec0
Potterf Mária <potterf@fld.czu.cz>
2025-06-27 16:24:48 +0200

    identify overlapping clusters between 2 years:

--- 06_analysis_between_years.R
+++ 06_analysis_between_years.R
@@ -27,8 +27,11 @@ library(purrr)
 # Read files --------------------------------------
 
 # 2023 
+dat23_subplot <- fread('outData/subplot_full_2023.csv')
+dat23_cluster <- fread('outData/df_cluster_2023.csv')
 
-
+# Save spatial subplot data with cluster IDs
+dat23_sf <- st_read("raw/collected_2023/dat_czechia_2023.gpkg")
 
 
 # 2025
@@ -37,3 +40,34 @@ dat25_cluster <- fread('outData/df_cluster_2025.csv')
 
 # Save spatial subplot data with cluster IDs
 dat25_sf <- st_read("outData/subplot_with_clusters_2025.gpkg")
+
+
+
+# Find overlapping clusters -----------------------------------
+st_geometry(dat23_sf) <- "geom"
+st_geometry(dat25_sf) <- "geom"
+
+
+# Summarize to cluster centroids
+centroids_2023 <- dat23_sf %>%
+  group_by(cluster) %>%
+  summarise(do_union = TRUE) %>%
+  st_centroid() %>%
+  rename(cluster_2023 = cluster)
+
+centroids_2025 <- dat25_sf %>%
+  group_by(cluster) %>%
+  summarise(do_union = TRUE) %>%
+  st_centroid() %>%
+  rename(cluster_2025 = cluster)
+
+
+# Match CRS
+centroids_2023 <- st_transform(centroids_2023, st_crs(centroids_2025))
+
+# Find clusters within 50 m
+overlaps <- st_join(centroids_2025, centroids_2023, join = st_is_within_distance, dist = 25) %>%
+  dplyr::filter(!is.na(cluster_2023)) %>%
+  mutate(common_cluster_ID = paste0("c_", cluster_2023, "_", cluster_2025))
+
+overlaps
