7db845d2c45d84dbb9b197f00a2aed9ae7c06c70
Potterf Mária <potterf@fld.czu.cz>
2026-08-27 07:56:55 +0200

    added plot coordinates

--- 12_get_plots_GFO.R
+++ 12_get_plots_GFO.R
@@ -20,6 +20,8 @@ models_with_cluster      <- readRDS("outModel/final_gam_models_with_cluster.rds"
 final_coefs              <- read.csv("outModel/final_gam_coefficients.csv")
 cluster_sensitivity_wide <- read.csv("outModel/cluster_sensitivity_wide.csv")
 
+plot_coords              <- readRDS("outModel/plot_coords.rds")
+
 # sanity check everything loaded as expected
 names(fin.models.all)
 names(models_with_cluster)
